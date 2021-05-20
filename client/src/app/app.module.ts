8fb11db8b176694112211539e04699ecb9ae4248
sroman28 <scott.roman28@t-mobile.com>
2021-05-20 14:07:54 -0400

    Adding in some frontend updates

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderBarComponent } from './header-bar/header-bar.component';
 import { AngularSetupComponent } from './angular-setup/angular-setup.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from "@angular/material/tabs";
+import { MatButtonModule } from '@angular/material/button';
 import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
 
 @NgModule({
@@ -20,7 +21,9 @@ import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
